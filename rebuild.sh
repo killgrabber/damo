f5bc429bff449cba095475d07ba2b9dc6de9e20f
Niklas Thieme <niklas.thieme@outlook.de>
2024-02-01 11:25:36 +0000

    build files in the rebuild script asweel

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,16 @@
 set -o errexit # exit on first error
 set -o nounset # unset variables are errors
 
+# Build latex files ...
+echo "Building latex files"
+cd src/ausarbeitung
+pdflatex main.tex
+cd ..
+cd ..
+cp src/ausarbeitung/main.pdf ausarbeitung.pdf
+
+echo "latex files finished..."
+
 if [ ${1:-foo} = "--clear-cache" ]; then
     sudo rm -r ~/.cpm-source-cache
 fi
@@ -15,3 +25,4 @@ rm -rf build
 cmake -S . -B build
 cd build
 make
+
