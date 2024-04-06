6fdf2686ebbe85365f52b9d5cb491859cadc0c8f
killgrabber <niklas.thieme@outlook.de>
2024-04-06 11:56:40 +0200

    refactor code

--- programm/pythonVis/visualize.py
+++ programm/pythonVis/visualize.py
@@ -10,6 +10,7 @@ max_correspondence_distance_fine = voxel_size * 1.5
 FILES = {"programm/pythonVis/LL_SLM-Probe_1ms_12mirometer_1_neu_ascii.ply",
          "programm/pythonVis/AM_Part_2_neu_ohne_infill.ply"}
 
+
 def display_inlier_outlier(cloud, ind):
     inlier_cloud = cloud.select_by_index(ind)
     outlier_cloud = cloud.select_by_index(ind, invert=True)
@@ -22,17 +23,19 @@ def display_inlier_outlier(cloud, ind):
                                       front=[0.4257, -0.2125, -0.8795],
                                       lookat=[2.6172, 2.0475, 1.532],
                                       up=[-0.0694, -0.9768, 0.2024])
-    
+
+
 def remove_outliers(cloud):
     print("remove statistical outliers....")
     cl, ind = cloud.remove_statistical_outlier(nb_neighbors=20,
-                                                        std_ratio=1)
+                                               std_ratio=1)
     inlier_cloud = cl.select_by_index(ind)
     cl, ind = inlier_cloud.remove_radius_outlier(nb_points=16, radius=0.05)
     inlier_cloud = cl.select_by_index(ind)
     amount = len(cloud.select_by_index(ind, invert=True).points)
     print("Points removed: ", amount)
     return inlier_cloud
+
 
 def pairwise_registration(source, target):
     print("Apply point-to-plane ICP")
@@ -48,6 +51,7 @@ def pairwise_registration(source, target):
         source, target, max_correspondence_distance_fine,
         icp_fine.transformation)
     return transformation_icp, information_icp
+
 
 def pairwise_registration(source, target):
     print("Apply point-to-plane ICP")
@@ -96,66 +100,69 @@ def full_registration(pcds, max_correspondence_distance_coarse,
                                                              uncertain=True))
     return pose_graph
 
+
 def load_point_clouds(voxel_size=0.0):
     pcds = []
     for file in FILES:
         pcd = o3d.io.read_point_cloud(file)
-        #pcd = remove_outliers(pcd)
+        # pcd = remove_outliers(pcd)
         pcd.estimate_normals()
-        #pcd_down = pcd.voxel_down_sample(voxel_size=voxel_size)
+        # pcd_down = pcd.voxel_down_sample(voxel_size=voxel_size)
         pcds.append(pcd)
         print("Loaded file: ", file)
         o3d.visualization.draw_geometries([pcd],
-                                        zoom=0.3412,
-                                        front=[0.4257, -0.2125, -0.8795],
-                                        lookat=[2.6172, 2.0475, 1.532],
-                                        up=[-0.0694, -0.9768, 0.2024])
+                                          zoom=0.3412,
+                                          front=[0.4257, -0.2125, -0.8795],
+                                          lookat=[2.6172, 2.0475, 1.532],
+                                          up=[-0.0694, -0.9768, 0.2024])
     return pcds
 
-def combine(): 
+
+def combine():
     voxel_size = 0.02
     pcds_down = load_point_clouds(voxel_size)
     o3d.visualization.draw_geometries(pcds_down,
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
+                                      zoom=0.3412,
+                                      front=[0.4257, -0.2125, -0.8795],
+                                      lookat=[2.6172, 2.0475, 1.532],
+                                      up=[-0.0694, -0.9768, 0.2024])
 
     print("Full registration ...")
     max_correspondence_distance_coarse = voxel_size * 15
     max_correspondence_distance_fine = voxel_size * 1.5
     with o3d.utility.VerbosityContextManager(
-        o3d.utility.VerbosityLevel.Debug) as cm:
+            o3d.utility.VerbosityLevel.Debug) as cm:
         pose_graph = full_registration(pcds_down,
-                                   max_correspondence_distance_coarse,
-                                   max_correspondence_distance_fine)
-        
+                                       max_correspondence_distance_coarse,
+                                       max_correspondence_distance_fine)
+
     print("Optimizing PoseGraph ...")
     option = o3d.pipelines.registration.GlobalOptimizationOption(
         max_correspondence_distance=max_correspondence_distance_fine,
         edge_prune_threshold=0.25,
         reference_node=0)
     with o3d.utility.VerbosityContextManager(
-        o3d.utility.VerbosityLevel.Debug) as cm:
+            o3d.utility.VerbosityLevel.Debug) as cm:
         o3d.pipelines.registration.global_optimization(
             pose_graph,
             o3d.pipelines.registration.GlobalOptimizationLevenbergMarquardt(),
             o3d.pipelines.registration.GlobalOptimizationConvergenceCriteria(),
             option)
-        
+
     print("Transform points and display")
     for point_id in range(len(pcds_down)):
         print(pose_graph.nodes[point_id].pose)
         pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
     o3d.visualization.draw_geometries(pcds_down,
-                                    zoom=0.3412,
-                                    front=[0.4257, -0.2125, -0.8795],
-                                    lookat=[2.6172, 2.0475, 1.532],
-                                    up=[-0.0694, -0.9768, 0.2024])
+                                      zoom=0.3412,
+                                      front=[0.4257, -0.2125, -0.8795],
+                                      lookat=[2.6172, 2.0475, 1.532],
+                                      up=[-0.0694, -0.9768, 0.2024])
 
-def oldStuff():     
-    FILE_NAME_1="programm/pythonVis/LL_SLM-Probe_1ms_12mirometer_1_neu_ascii.ply"
-    FILE_NAME_2="programm/pythonVis/LL_SLM-Probe_1ms_12mirometer_2_neu_ascii.ply"
+
+def oldStuff():
+    FILE_NAME_1 = "programm/pythonVis/LL_SLM-Probe_1ms_12mirometer_1_neu_ascii.ply"
+    FILE_NAME_2 = "programm/pythonVis/LL_SLM-Probe_1ms_12mirometer_2_neu_ascii.ply"
 
     print("read point cloud " + FILE_NAME_1 + "...")
     pcd_1 = o3d.io.read_point_cloud(FILE_NAME_1)
@@ -166,12 +173,11 @@ def oldStuff():
     clean_2 = remove_outliers(pcd_2)
 
     o3d.visualization.draw_geometries([clean_1, clean_2],
-                                        zoom=0.3412,
-                                        front=[0.4257, -0.2125, -0.8795],
-                                        lookat=[2.6172, 2.0475, 1.532],
-                                        up=[-0.0694, -0.9768, 0.2024])
+                                      zoom=0.3412,
+                                      front=[0.4257, -0.2125, -0.8795],
+                                      lookat=[2.6172, 2.0475, 1.532],
+                                      up=[-0.0694, -0.9768, 0.2024])
 
 
 if __name__ == "__main__":
     combine()
-
